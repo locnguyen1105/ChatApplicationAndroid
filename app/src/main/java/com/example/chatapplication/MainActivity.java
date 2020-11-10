b50994f26c815c0adacd48235308fde19e495e54
nhatanh1999 <anh0949709018>
2020-11-10 19:55:26 +0700

    nhatanh push

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.chatapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button _logout;
+    TextView _user, _email, _profilename;
+    CircleImageView _profileimage;
+    FirebaseUser firebaseUser;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+
+        _profileimage = findViewById(R.id.profile_image);
+        _profilename = findViewById(R.id.profile_name);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        Query query = databaseReference.orderByChild("Email").equalTo(firebaseUser.getEmail());
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    _profilename.setText(ds.child("Username").getValue().toString());
+                    _profileimage.setImageResource(R.drawable.anhdepa);
+                    //Glide.with(MainActivity.this).load().into(_profileimage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        BottomNavigationView navigationView = findViewById(R.id.bottomnav);
+        navigationView.setOnNavigationItemSelectedListener(selectListioner);
+
+        ChatFragment chatFragment = new ChatFragment();
+        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+        ft1.replace(R.id.content, chatFragment, "");
+        ft1.commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectListioner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_chat:
+                            ChatFragment chatFragment = new ChatFragment();
+                            FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                            ft1.replace(R.id.content, chatFragment, "");
+                            ft1.commit();
+                            return true;
+                        case R.id.nav_profile:
+                            ProfileFragment profileFragment = new ProfileFragment();
+                            FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+                            ft2.replace(R.id.content, profileFragment, "");
+                            ft2.commit();
+                            return true;
+                        case R.id.nav_friend:
+                            UsersFragment usersFragment = new UsersFragment();
+                            FragmentTransaction ft3 = getSupportFragmentManager().beginTransaction();
+                            ft3.replace(R.id.content, usersFragment, "");
+                            ft3.commit();
+                            return true;
+
+                    }
+                    return false;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+        }
+        return false;
+    }
+
 }
