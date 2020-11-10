054bce78513269933fcaa2c81d1cad73a1f9176e
nguyen xuan loc <locnx1105@gmail.com>
2020-11-10 21:45:44 +0700

    edit model

--- app/src/main/java/com/example/chatapplication/Model/Account.java
+++ app/src/main/java/com/example/chatapplication/Model/Account.java
@@ -7,37 +7,37 @@ import java.util.HashMap;
 
 
 public class Account implements Serializable {
-    public String ID;
-    public String Username;
-    public String Email;
-    public String Password;
+    private String uid;
+    private String Username;
+    private String Password;
+    private String Email;
 
     public Account() {
     }
 
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public Account(String id, String username, String email, String password) {
-        ID = id;
-        Username = username;
-        Email = email;
-        Password = password;
+    public Account(String uid, String Username, String Password, String Email) {
+        this.uid = uid;
+        this.Username = Username;
+        this.Password = Password;
+        this.Email = Email;
     }
 
     @Exclude
     public HashMap<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("uid", ID);
+        result.put("uid", uid);
         result.put("Username", Username);
         result.put("Email", Email);
         result.put("Password", Password);
         return result;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
     public String getUsername() {
@@ -48,20 +48,20 @@ public class Account implements Serializable {
         Username = username;
     }
 
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
     public String getPassword() {
         return Password;
     }
 
     public void setPassword(String password) {
         Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
     }
 }
 
